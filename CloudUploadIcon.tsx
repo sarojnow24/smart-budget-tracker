c80e3b620451aef85ece87120ad286c0ef552e8d
sarojnow24 <sarojnow24@gmail.com>
2026-02-16 00:28:15 +0400

    Create CloudUploadIcon.tsx

---
+++ CloudUploadIcon.tsx
@@ -0,0 +1,24 @@
+// src/components/CloudUploadIcon.tsx
+import React from 'react';
+
+const CloudUploadIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    className="lucide lucide-cloud-upload-icon"
+    {...props}
+  >
+    <path d="M12 13v8" />
+    <path d="M4 14.899A7 7 0 1 1 15.71 8h1.79a4.5 4.5 0 0 1 2.5 8.242" />
+    <path d="m8 17 4-4 4 4" />
+  </svg>
+);
+
+export default CloudUploadIcon;
